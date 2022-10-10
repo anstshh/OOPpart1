dc9283f522ca8f253e373713131d948c2ce78b71
Admin <anstdor@gmail.com>
2022-10-10 17:55:04 +0300

    Changes class Human, constructor was used

--- src/Human.java
+++ src/Human.java
@@ -3,4 +3,11 @@ public class Human {
     public String name;
     public String city;
     public String position;
+
+    public Human(int age, String name, String city, String position) {
+        this.age = age;
+        this.name = name;
+        this.city = city;
+        this.position = position;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,11 @@ public class Main {
 
         int currentYear = 2022;
 
-        Human maksim = new Human();
-        maksim.name = "Максим";
-        maksim.age = 35;
-        maksim.city = "Минск";
-        maksim.position = "Бренд-менеджер";
+        Human maksim = new Human(35, "Максим", "Минск", "Бренд-менеджер");
+        Human anya = new Human(29, "Аня", "Москва", "Методист образовательных программ");
+        Human katya = new Human(28, "Катя", "Калининград", "Продакт-менеджер");
+        Human artyom = new Human(27, "Артем", "Москва", "Директор по развитию бизнеса");
 
-        Human anya = new Human();
-        anya.name = "Аня";
-        anya.age = 29;
-        anya.city = "Москва";
-        anya.position = "Методист образовательных программ";
-
-        Human katya = new Human();
-        katya.name = "Катя";
-        katya.age = 28;
-        katya.city = "Калининград";
-        katya.position = "Продакт-менеджер";
-
-        Human artyom = new Human();
-        artyom.name = "Катя";
-        artyom.age = 27;
-        artyom.city = "Москва";
-        artyom.position = "Директор по развитию бизнеса";
 
         System.out.println("Привет! Меня зовут "+maksim.name+". Я из города "+maksim.city+". Я родился в  "+(currentYear - maksim.age)+" году. Я работаю на должности "+maksim.position+".Будем знакомы!");
         System.out.println("Привет! Меня зовут "+anya.name+". Я из города "+anya.city+". Я родился в  "+(currentYear - anya.age)+" году. Я работаю на должности "+anya.position+". Будем знакомы!");
