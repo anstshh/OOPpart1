439880cddbb13c1f930e801259cb170f40a91638
Admin <anstdor@gmail.com>
2022-10-13 18:53:00 +0300

    Next lesson additional changes

--- src/Human.java
+++ src/Human.java
@@ -1,7 +1,7 @@
 public class Human {
-    public int age;
+    private int age;
     public String name;
-    public String city;
+    private String city;
     public String position;
 
     public Human(int age, String name, String city, String position) {
@@ -24,6 +24,30 @@ public class Human {
             this.position = "Информация не указана";
         } else {
             this.position = position;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age<0) {
+            this.age = 0;
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        if (city==null) {
+            this.city = "Информация не указана";
+        } else {
+            this.city = city;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,17 @@ public class Main {
         Human anya = new Human(29, "Аня", "Москва", "Методист образовательных программ");
         Human katya = new Human(28, "Катя", "Калининград", "Продакт-менеджер");
         Human artyom = new Human(27, "Артем", "Москва", "Директор по развитию бизнеса");
+        Human vladimir = new Human(21, "Владимир", "Казань", null);
 
+        printInfo(maksim);
+        printInfo(anya);
+        printInfo(katya);
+        printInfo(artyom);
+        printInfo(vladimir);
+    }
 
-        System.out.println("Привет! Меня зовут "+maksim.name+". Я из города "+maksim.city+". Я родился в  "+(currentYear - maksim.age)+" году. Я работаю на должности "+maksim.position+".Будем знакомы!");
-        System.out.println("Привет! Меня зовут "+anya.name+". Я из города "+anya.city+". Я родился в  "+(currentYear - anya.age)+" году. Я работаю на должности "+anya.position+". Будем знакомы!");
-        System.out.println("Привет! Меня зовут "+katya.name+". Я из города "+katya.city+". Я родился в  "+(currentYear - katya.age)+" году. Я работаю на должности "+katya.position+". Будем знакомы!");
-        System.out.println("Привет! Меня зовут "+artyom.name+". Я из города "+artyom.city+". Я родился в  "+(currentYear - artyom.age)+" году. Я работаю на должности "+artyom.position+". Будем знакомы!");
+    private static void printInfo(Human human) {
+        System.out.println("Привет! Меня зовут "+human.name+". Я из города "+human.getCity()+". Я родился в  "+human.getAge()+" году. Я работаю на должности "+human.position+".Будем знакомы!");
+
     }
 }
